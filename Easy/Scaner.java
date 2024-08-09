d738f1c6e891c752344825b8efe5a040e3d1a503
Visal Vishmitha Alwis <106392603+vishmitha1@users.noreply.github.com>
2024-08-09 20:56:33 +0530

    Day1

---
+++ Easy/Scaner.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Scaner {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+      
+        int int1 = scan.nextInt();
+        int int2 = scan.nextInt();
+        int int3 = scan.nextInt();
+     
+        scan.close();
+
+        System.out.println(int1+"\n"+int2+"\n"+int3);
+        
+        
+    }
+}
